f02b1c1004342dd008550074e10de4b821c1ecd4
Sid Lambert <sid@deepredanimation.com>
2021-10-03 15:55:17 -0400

    Added legend ref card

---
+++ exo/resources/views/reference_card/legend.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Legend</title>
+    <link rel="stylesheet" href="/css/foundation/foundation.css">
+    <link rel="stylesheet" href="/css/foundation/app.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/Glyphter.css">
+    <link rel="stylesheet" href="/css/exo.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="row" style="background: url('/img/art/starfield.jpg');
+          background-repeat: no-repeat;background-position:top;padding:0px;max-width: 108rem;">
+      <div class="glow" style="width: 1579px;height:985px;margin:70px 0px 70px 59px;border-radius:15px;">
+        <h1 style='margin-left:10px;'>
+          Legend
+        </h1>
+
+        <table id='legend'>
+          <tr>
+            <td><img src='/img/art/symbols/food.png' style='width:60px;' /></td>
+            <td>Food</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/metal.png' style='width:60px;' /></td>
+            <td>Metal</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/food.png' style='width:60px;' /></td>
+            <td>Fuel</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>
+              <span class="fa-stack fa-lg">
+                <i class="exo-fuel fa-stack-1x"></i>
+                <i class="fa-stack-1x cost">&#x2197;</i>
+              </span>
+            </td>
+            <td>Launch Cost</td>
+            <td>Fuel cost to launch off a world</td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/metal.png' style='width:60px;' /></td>
+            <td>Metal</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/water.png' style='width:60px;' /></td>
+            <td>Water</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>
+              <span class="fa-stack fa-lg">
+                <i class="exo-coin fa-stack-1x"></i>
+              </span>
+            </td>
+            <td>Coin</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>
+              <span class="fa-stack fa-lg">
+                <i class="exo-coin fa-stack-1x"></i>
+                <i class="fa-stack-1x cost" style='margin-top:3px;'>&#x2194;</i>
+              </span>
+            </td>
+            <td>Interplanetary transfer cost</td>
+            <td>Coin cost in distance between planets</td>
+          </tr>
+          <tr>
+            <td>
+              <span class="fa-stack fa-lg">
+                <i class="exo-coin fa-stack-1x"></i>
+                <i class="fa-stack-1x cost" style='margin-top:3px;'>&#x2191;</i>
+              </span>
+            </td>
+            <td>Interstellar transfer cost</td>
+            <td>Coin cost in distance between stars</td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/eternal-light.png' style='width:60px;' /></td>
+            <td>Peak of Eternal Light</td>
+            <td>
+              Provides
+              <span class="fa-stack fa-lg">
+                <i class="exo-coin fa-stack-1x"></i>
+                <i class="fa-stack-1x cost" style='margin-top:5px;'>1</i>
+              </span>
+              in the habitable zone or closer
+            </td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/cave.png' style='width:60px;' /></td>
+            <td>Cave</td>
+            <td>
+              <span class="fa-stack fa-lg">
+                <i class="exo-metal fa-stack-1x"></i>
+                <i class="fa-stack-1x cost" style='margin-top:5px;'>1</i>
+              </span>
+              <span>Discount when settling a colony</span>
+            </td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/magnetic-shield.png' style='width:60px;' /></td>
+            <td>Magnetic Shield</td>
+            <td>
+              <span class="fa-stack fa-lg">
+                <i class="exo-metal fa-stack-1x"></i>
+                <i class="fa-stack-1x cost" style='margin-top:5px;'>1</i>
+              </span>              
+              <span>Discount when settling a colony</span>
+            </td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/magnetic-storm.png' style='width:60px;' /></td>
+            <td>Magnetic Storm</td>
+            <td>
+              <span class="fa-stack fa-lg">
+                <i class="exo-metal fa-stack-1x"></i>
+                <i class="fa-stack-1x cost" style='margin-top:5px;'>1</i>
+              </span>
+              <span>Extra cost when settling a colony</span>
+            </td>
+          </tr>
+          <tr>
+            <td><img src='/img/art/symbols/victory-points.png' style='width:60px;' /></td>
+            <td>Victory Point</td>
+            <td>Gain 16 to win</td>
+          </tr>
+        </table>
+
+      </div>
+    </div>
+
+    <script src="/js/foundation/vendor/jquery.js"></script>
+    <script src="/js/foundation/vendor/what-input.js"></script>
+    <script src="/js/foundation/vendor/foundation.js"></script>
+    <script src="/js/foundation/app.js"></script>
+  </body>
+</html>
